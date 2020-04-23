7d00a9bd4ed98e4e752363c9f024bf65dc3c07cc
Phillip Corrêa Mittmann <phillip.mittmann@gmail.com>
2020-04-23 18:09:09 -0300

    Ajustes Memory

--- src/main/java/br/com/jvos/Memory.java
+++ src/main/java/br/com/jvos/Memory.java
@@ -5,46 +5,33 @@ import java.util.Arrays;
 public class Memory {
 
     private int[] row;
-    private int nextPartition = 0;
-    private int timesAllocated;
-    private int partitionStart;
+
     private int partitionAmount;
-    private boolean[] freePartitions;
+
+    private boolean[] allocated;
 
     public Memory(int size) {
         this.row = new int[size];
+
         this.partitionAmount = 4;
-        this.timesAllocated = 0;
-        this.partitionStart = this.nextPartition;
-        this.nextPartition = size / this.partitionAmount + this.nextPartition;
-        freePartitions = new boolean[this.row.length / partitionAmount];
-        Arrays.fill(this.freePartitions, true);
+
+        allocated = new boolean[partitionAmount];
+        Arrays.fill(this.allocated, false);
     }
 
     public int getRow(int position) {
         return row[position];
     }
 
-    public void setRow(int position, int value) {
-        this.row[position + (this.timesAllocated * this.row.length / this.partitionAmount)] = value;
-    }
+    public void setRow(int position, int value) { this.row[position] = value; }
 
     public void printBlock() {
-        for (int i = 0;  i < this.nextPartition; i++) {
-            if (i >= this.partitionStart) {
+        for (int i = 0;  i < 10; i++) {
                 System.out.println("[" + i + "]\t-> " + row[i]);
-            }
-        }
-        System.out.println("==================================");
-
-        allocateMemory();
-    }
-
-    public void allocateMemory() {
-        if(this.partitionStart < row.length) {
-            this.partitionStart = this.nextPartition;
-            this.nextPartition = row.length / 4 + this.nextPartition;
-            this.timesAllocated++;
         }
     }
+
+    public void setPartitionAllocatedValue(int position, boolean value) { this.allocated[position] = value; }
+
+    public boolean partitionIsAllocated(int position) { return this.allocated[position]; }
 }
